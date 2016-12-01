21a90a5371d660438a5d0b68dc262f94da2aede1
Kevin Finley <kevin@>
2016-12-01 12:04:32 -0800

    add #save method and pass

--- lib/user.rb
+++ lib/user.rb
@@ -1,8 +1,11 @@
 class User
   attr_reader(:name)
-
+@@users_arr = []
   def initialize(attributes)
     @name = attributes.fetch(:name)
   end
 
+  def save
+    @@users_arr.push(self)
+  end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -8,4 +8,12 @@ describe(User) do
       expect(test_user.name()).to(eq('Kevin'))
     end
   end
+
+  describe('#save') do
+    it "will save the user into an array" do
+
+      test_user = User.new(:name => 'Kevin')
+      expect(test_user.save()).to(eq([test_user]))
+    end
+  end
 end
