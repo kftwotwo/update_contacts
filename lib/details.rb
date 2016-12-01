ecd6e1765c65655459f8426c29c8d4eb9983b583
Kevin Finley <kevin@>
2016-12-01 09:54:25 -0800

    add part 2 to initialize

--- lib/details.rb
+++ lib/details.rb
@@ -1,6 +1,7 @@
 class Detail
-  attr_reader(:address)
+  attr_reader(:address, :home_phone)
   def initialize(attributes)
     @address = attributes.fetch(:address)
+    @home_phone = attributes.fetch(:home_phone)
   end
 end
--- spec/details_spec.rb
+++ spec/details_spec.rb
@@ -3,9 +3,10 @@ require "rspec"
 
 describe(Detail) do
   describe("#intialize") do
-    it "will return address" do
-      test_detail = Detail.new({:address => "2915 hwy 78"})
+    it "will return address, home_phone" do
+      test_detail = Detail.new({:address => "2915 hwy 78", :home_phone => "123-456-7890"})
       expect(test_detail.address()).to(eq("2915 hwy 78"))
+      expect(test_detail.home_phone()).to(eq("123-456-7890"))
     end
   end
 end
