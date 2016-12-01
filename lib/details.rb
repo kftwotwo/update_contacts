6e9f627054a6f228b8ab48e809813703daccd429
Kevin Finley <kevin@>
2016-12-01 09:58:57 -0800

    add part 4 for initialize

--- lib/details.rb
+++ lib/details.rb
@@ -1,8 +1,9 @@
 class Detail
-  attr_reader(:address, :home_phone, :cell_phone)
+  attr_reader(:address, :home_phone, :cell_phone, :email)
   def initialize(attributes)
     @address = attributes.fetch(:address)
     @home_phone = attributes.fetch(:home_phone)
     @cell_phone = attributes.fetch(:cell_phone)
+    @email = attributes.fetch(:email)
   end
 end
