c7c68fa08383c6992d24a29e47b0a9c5410e92c5
Kevin Finley <kevin@>
2016-12-01 12:10:58 -0800

    add .clear method and pass

--- lib/user.rb
+++ lib/user.rb
@@ -8,4 +8,12 @@ class User
   def save
     @@users_arr.push(self)
   end
+
+  define_singleton_method(:clear) do
+    @@users_arr = []
+  end
+
+  # define_singleton_method(:all) do
+  #   @@users_arr
+  # end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -11,9 +11,25 @@ describe(User) do
 
   describe('#save') do
     it "will save the user into an array" do
-
       test_user = User.new(:name => 'Kevin')
       expect(test_user.save()).to(eq([test_user]))
     end
   end
+
+  describe('.clear') do
+    it "will clear the array" do
+      test_user = User.new(:name => 'Kevin').save()
+      expect(User.clear()).to(eq([]))
+    end
+  end
+
+  # describe(".all") do
+  #   it "will display all users" do
+  #     test_user = User.new(:name => '')
+  #     test_user.save()
+  #     test_user2 = User.new(:name => 'Kevin')
+  #     test_user2.save()
+  #     expect(User.all()).to(eq([test_user, test_user2]))
+  #   end
+  # end
 end
